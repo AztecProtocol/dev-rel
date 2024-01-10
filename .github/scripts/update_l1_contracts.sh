4635fc2fb2ea6f19441913c4f7fd37ed2a8089cc
Cat McGee <helloworld@mcgee.cat>
2024-01-10 14:15:28 +0900

    testing workflow

--- .github/scripts/update_l1_contracts.sh
+++ .github/scripts/update_l1_contracts.sh
@@ -11,42 +11,40 @@ if [ ! -d "tmp" ]; then
     error_exit "tmp directory not found. Did update_aztec_contracts.sh run successfully?"
 fi
 
-# Base directory to search for .sol files
-base_dirs=("./tutorials" "./workshops")
+# Directory containing the aztec-packages L1 contracts
+source_contracts_dir="tmp/l1-contracts/test" 
 
-
-# Directory containing the L1 contracts
-l1_contracts_dir="tmp/l1-contracts/test"  # Adjust to the correct subdirectory path in tmp
+# Base directories
+target_dirs=("./tutorials" "./workshops")
 
 # Process each base directory
-for base_dir in "${base_dirs[@]}"; do
-    echo "Processing directory: $base_dir"
+for target_dir in "${target_dirs[@]}"; do
+    echo "Processing directory: $target_dir"
 
-    # Loop through each .sol file found in the base directory
-    find "$base_dir" -name "*.sol" | while read -r sol_file; do
-        echo "Processing $sol_file..."
+    # Loop through each .sol file found in the aztec-packages directory
+    find "$source_contracts_dir" -name "*.sol" | while read -r source_file; do
+        echo "Processing $source_file..."
 
-        # Extract the filename without the path
-        filename=$(basename "$sol_file")
+        # Extract the filename
+        filename=$(basename "$source_file")
 
-        # Find the equivalent .sol file in the l1-contracts directory
-        l1_equivalent=$(find "$l1_contracts_dir" -name "$filename")
+        # Find the equivalent .sol file in the dev-rel dir
+        target_file=$(find "$target_dir" -name "$filename")
 
-        if [ -z "$l1_equivalent" ]; then
-            echo "No equivalent file found for $filename in L1 contracts directory."
+        if [ -z "$target_file" ]; then
+            echo "No equivalent file found for $filename in $target_dir directory."
             continue
         fi
 
-        echo "Found equivalent L1 contract: $l1_equivalent"
+        echo "Found target file: $target_file"
 
-        # Replace the code in the L1 equivalent with the code from the found .sol file
-        cp "$sol_file" "$l1_equivalent"
-        echo "Replaced $sol_file"
+        # Replace the code in dev-rel repo with the code from the aztec-packages .sol file
+        cp "$source_file" "$target_file"
+        echo "Updated $target_file"
 
-        # Remove comments from the L1 contract file
-        sed -i '/^[ \t]*\/\//d' "$l1_equivalent"  # Removes single-line comments
-        sed -i '/\/\*/,/\*\//d' "$l1_equivalent" # Removes block comments
-        echo "Removed comments from $sol_file"
+       # Remove 'docs' comments
+        sed -i '/[ \t]*\/\/ docs:.*/d' "$target_file"
+        echo "Docs comments removed from $target_file"
     done
 done
 
