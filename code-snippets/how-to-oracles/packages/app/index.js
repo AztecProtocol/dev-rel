f66adc4f790bb50c53167ebd169419338b046ecd
José Pedro Sousa <outgoing@zpedro.dev>
2024-01-12 11:26:10 +0000

    adding code snippet used in the how_to_oracles page on the Noir docs

---
+++ code-snippets/how-to-oracles/packages/app/index.js
@@ -0,0 +1,40 @@
+import { BarretenbergBackend } from "@noir-lang/backend_barretenberg";
+import { Noir } from "@noir-lang/noir_js";
+import project from "../circuit/target/project.json" assert { type: "json" };
+import { JSONRPCClient } from "json-rpc-2.0";
+
+const client = new JSONRPCClient((jsonRPCRequest) => {
+	return fetch("http://localhost:5555", {
+		method: "POST",
+		headers: {
+			"content-type": "application/json",
+		},
+		body: JSON.stringify(jsonRPCRequest),
+	}).then((response) => {
+		if (response.status === 200) {
+			return response
+				.json()
+				.then((jsonRPCResponse) => client.receive(jsonRPCResponse));
+		} else if (jsonRPCRequest.id !== undefined) {
+			return Promise.reject(new Error(response.statusText));
+		}
+	});
+});
+
+const oracleResolver = async (name, input) => {
+	const oracleReturn = await client.request(name, [
+		{ Array: input[0].map((i) => ({ inner: i })) },
+	]);
+	return [oracleReturn.values[0].Array.map((x) => x.inner)];
+};
+
+async function main() {
+	const backend = new BarretenbergBackend(project);
+	const noir = new Noir(project, backend);
+
+	const input = { input: [1, 2], target: [1, 4] };
+	const proof = await noir.generateFinalProof(input, oracleResolver);
+	console.log(proof);
+}
+
+main();
---
+++ code-snippets/how-to-oracles/packages/rpc_server/index.js
@@ -0,0 +1,29 @@
+import { JSONRPCServer } from "json-rpc-2.0";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const server = new JSONRPCServer();
+server.addMethod("getSquared", async (params) => {
+	const values = params[0].Array.map(({ inner }) => {
+		return { inner: `${inner * inner}` };
+	});
+	return { values: [{ Array: values }] };
+});
+
+app.post("/", (req, res) => {
+	const jsonRPCRequest = req.body;
+	server.receive(jsonRPCRequest).then((jsonRPCResponse) => {
+		if (jsonRPCResponse) {
+			res.json(jsonRPCResponse);
+		} else {
+			res.sendStatus(204);
+		}
+	});
+});
+
+app.listen(5555);
