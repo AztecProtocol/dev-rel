53b85e7e59f4fda3a2d1738d016878d752a4f40d
josh crites <critesjosh@gmail.com>
2024-10-07 10:37:47 -0400

    Delete tutorials directory

--- tutorials/README.md
+++
@@ -1,3 +0,0 @@
-# Tutorials
-
-🧑‍💻 [Write and deploy a private token contract with Aztec.nr and Aztec.js](simple-private-token)
--- tutorials/token-bridge-e2e/packages/src/test/fixtures/utils.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Sleep for a given number of milliseconds.
- * @param ms - the number of milliseconds to sleep for
- */
-export function delay(ms: number): Promise<void> {
-    return new Promise<void>(resolve => setTimeout(resolve, ms));
-  }
